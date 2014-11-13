f707cef4bec3299e0d35729e2b216942ed2c943e
Michelle <michelle.senar@brightscope.com>
2014-11-13 14:52:07 -0800

    Files were kinda deleted, but more like shuffled around to be organized

--- finder/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import Kitty, Shelter
-
-admin.site.register(Kitty)
-admin.site.register(Shelter)
--- finder/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import Kitty
-
-class AddCat(forms.ModelForm):
-
-    class Meta:
-        model = Kitty
-        fields = ('name', 'location', 'gender', 'age', 'breed', 'about', 'status','photo')
