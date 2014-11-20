994f5d56da1d5c18d8581bfa54effef2babd2544
Michelle <michelle.senar@brightscope.com>
2014-11-20 10:28:59 -0800

    modified setup.py and tox.ini to have the proper requirements, to pip install the project

---
+++ setup.py
@@ -0,0 +1,50 @@
+import platform
+from setuptools import setup, find_packages
+from distutils.core import Command
+
+class DisabledCommands(Command): #not sure if needed, but in the datanav one
+  user_options = []
+
+  def initialize_options(self):
+    raise Exception('This command is disabled')
+
+  def finalize_options(self):
+    raise Exception('This command is disabled')
+
+requirements = [
+    'Django==1.7.1',
+    'Pillow==2.3.0',
+    'dj_database_url',
+]
+
+testing_requirements = [
+    'mock==1.0.1',
+    'py==1.4.26',
+    'pytest==2.6.4',
+    'pytest-django==2.7.0',
+    'tox',
+]
+
+development_requirements = [ #identical to datanav for now
+    'ipdb',
+    'ipython',
+]
+
+
+setup(name='friendmemeow',
+      version='0.1.0',
+      description='Michelle Senar Personal Project',
+      author='Michelle Senar',
+      author_email='michelle.senar@brightscope.com',
+      license='All Rights Reserved',
+      install_requires=requirements,
+      extras_require={
+            'dev': development_requirements + testing_requirements,
+            'test': testing_requirements,
+      },
+      #scripts=['manage.py'], #my manage.py is in the same directory as this setup.py
+      packages=['friendmemeow'],
+      include_package_data=True,
+      cmdclass={'register': DisabledCommands,
+                'upload': DisabledCommands}
+      )
