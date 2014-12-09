411ae4917cf1c28e4270ef4ff0b988392082b78f
Michelle <michelle.senar@brightscope.com>
2014-12-09 08:46:36 -0800

    controller, directive, templates, and script.js file all in place

--- friendmemeow/finder/urls.py
+++ friendmemeow/finder/urls.py
@@ -1,27 +1,25 @@
 from django.conf import settings
-from django.conf.urls import patterns, include, url,static
+from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+
+
 from . import views
 from rest_framework import routers
+
+class HomeView(TemplateView):
+    template_name = "finder/ready_cats.html"
 
 router = routers.DefaultRouter()
 router.register(r'kitty', views.CatList)
 router.register(r'shelters', views.ShelterList)
 
 
-
-# urlpatterns = patterns('',
-#     url(r'^$', views.ready_cats),
-#     url(r'^kitty/(?P<pk>[0-9]+)/$', views.cat_detail),
-#     url(r'^kitty/add/$', views.cat_form, name='add_cat'),
-#     url(r'^kitty/(?P<pk>\d+)/edit/$', views.cat_form, name='edit_cat'),
-#     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-# ) + static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns = [
-    url(r'^', include(router.urls)),
+urlpatterns = patterns('',
+    url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^kitty/(?P<pk>[0-9]+)/$', views.CatDetail),
-]
+    url(r'^$', HomeView.as_view()),
+)
 
 if settings.DEBUG:
     urlpatterns += patterns('',
