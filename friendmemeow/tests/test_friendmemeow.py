b504422a5d7d25fef2b0371a22c8cceca3dc5020
Michelle <michelle.senar@brightscope.com>
2014-12-15 14:01:36 -0800

    fixed python unit tests to only test the arithmetic functions in functions.py; url resolve tests gone due to old django views being gone

--- friendmemeow/tests/test_friendmemeow.py
+++ friendmemeow/tests/test_friendmemeow.py
@@ -7,7 +7,7 @@ from django.core.urlresolvers import resolve
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from finder.functions import half_age_plus_seven, monthly_food_cost
 from finder.models import Kitty
-from finder.views import ready_cats, cat_detail
+# from finder.views import ready_cats, cat_detail
 
 
 class KittyFactory(factory.Factory):
@@ -32,12 +32,12 @@ class KittyTestCase(unittest.TestCase):
         food_cost = monthly_food_cost(self.cat.weight)
         self.assertEqual(round(food_cost, 2), 11.57)
 
-    def test_root_url_resolve_to_ready_cats_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, ready_cats)
-
-    def test_kitty_url_resolve_to_cat_detail_view(self):
-        found = resolve('/kitty/1/')
-        self.assertEqual(found.func, cat_detail)
+    # def test_root_url_resolve_to_ready_cats_view(self):
+    #     found = resolve('/')
+    #     self.assertEqual(found.func, ready_cats)
+    #
+    # def test_kitty_url_resolve_to_cat_detail_view(self):
+    #     found = resolve('/kitty/1/')
+    #     self.assertEqual(found.func, cat_detail)
 
 
